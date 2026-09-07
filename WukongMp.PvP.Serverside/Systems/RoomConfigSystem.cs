e055efeccb9243a13ec9239a06c08070da8fd608
Jakub Janaszkiewicz <janaszkiewicz.jakub@gmail.com>
2026-09-07 14:17:54 +0200

    Re-apply config file periodically

---
+++ WukongMp.PvP.Serverside/Systems/RoomConfigSystem.cs
@@ -0,0 +1,14 @@
+﻿using ReadyM.Relay.Server.Sdk.Ecs.Systems;
+
+namespace WukongMp.PvP.Serverside.Systems;
+
+/// <summary>
+/// Drives <see cref="RoomConfigApplier" />, which does its own rate limiting.
+/// </summary>
+public sealed class RoomConfigSystem(RoomConfigApplier applier) : ModSystemBase
+{
+    protected override void OnUpdate(UpdateTick tick)
+    {
+        applier.Poll(tick.Time);
+    }
+}
